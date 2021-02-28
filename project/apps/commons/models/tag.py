534e9afd96bc02fc8d583b9e518a1704c927f4ee
Ruslan <rus.muratow@gmail.com>
2021-02-28 13:27:20 +0300

    Some changes

--- project/apps/commons/models/tag.py
+++ project/apps/commons/models/tag.py
@@ -2,13 +2,14 @@ from django.db import models
 
 
 class TagMixin(models.Model):
-    title = models.CharField('Имя', max_length=50)
-    slug = models.SlugField('Слаг', max_length=100)
-    references = models.PositiveIntegerField('Кол-во упоминаний', blank=True, default=0)
-    cleaned_name = models.CharField('Уникальное имя', unique=True, blank=True, max_length=50)
+    tag_title = models.CharField('Имя', max_length=50)
+    tag_slug = models.SlugField('Слаг', max_length=100)
+    tag_references = models.PositiveIntegerField('Кол-во упоминаний', blank=True, default=0)
+    tag_cleaned_name = models.CharField('Уникальное имя', unique=True, blank=True, max_length=50)
 
+    # Под вопросом
     def save(self, *args, **kwargs):
-        self.cleaned_name = self.title.lower().replace(' ', '')
+        self.tag_cleaned_name = self.tag_title.lower().replace(' ', '')
         super(TagMixin, self).save(*args, **kwargs)
 
     class Meta:
